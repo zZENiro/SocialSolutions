c8042bf1689f2e19c5230f1ca6fca60c3ee6c9e2
Никита <soloistcsgo@yandex.ru>
2020-10-31 16:57:31 +0300

    profileVM changed

--- SocialSolutions/Models/ViewModels/ProfileViewModel.cs
+++ SocialSolutions/Models/ViewModels/ProfileViewModel.cs
@@ -23,53 +23,5 @@ namespace SocialSolutions.Models.ViewModels
         public Location Location { get; set; }
 
         public string MobilePhone { get; set; }
-
-        public IEnumerable<Community> OwnCommunities { get; set; }
-
-        public IEnumerable<Document> Documents { get; set; }
-        
-        public IEnumerable<Community> Communities { get; set; }
-
-        public IEnumerable<Permit> Permits { get; set; }
-
-        public IEnumerable<Role> Roles { get; set; }
-
-        public IEnumerable<Group> Groups { get; set; }
-
-        public IEnumerable<UserAlbum> Albums { get; set; }
-
-        public IEnumerable<Hobby> Hobbies { get; set; }
-
-        public IEnumerable<Skill> Skills { get; set; }
-
-        public IEnumerable<Event> Events { get; set; }
-
-        public static implicit operator ProfileViewModel(Account acc)
-        {
-            var res =  new ProfileViewModel()
-            {
-                OwnCommunities = acc.User.OwnCommunities,
-                Communities = acc.User.Communities.Select(prop => prop.Community),
-                Account = new AccountViewModel() { Id = acc.Id, Login = acc.Login, Password = acc.Password },
-                ProfileId = acc.User.Id,
-                Name = acc.User.Name,
-                SecondName = acc.User.SecondName,
-                AboutMe = acc.User.AboutMe,
-                Birthdate = acc.User.Birthdate,
-                Gender = acc.User.Gender,
-                Location = acc.User.Location,
-                MobilePhone = acc.User.MobilePhone,
-                Documents = acc.User.Documents,
-                Permits = acc.User.Permits.Select(prop => prop.Permit),
-                Roles = acc.User.Roles.Select(prop => prop.Role),
-                Groups = acc.User.Groups.Select(prop => prop.Group),
-                Albums = acc.User.Albums,
-                Hobbies = acc.User.Hobbies.Select(prop => prop.Hobby),
-                Skills = acc.User.Skills.Select(prop => prop.Skill),
-                Events = acc.User.Events.Select(prop => prop.Event)
-            };
-
-            return res;
-        }
     }
 }
