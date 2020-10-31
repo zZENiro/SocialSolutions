4dfcd6433ae748e5c58b82fa0f883fe1e1307852
Никита <soloistcsgo@yandex.ru>
2020-10-31 16:58:34 +0300

    profile back

--- SocialSolutions/Models/User.cs
+++ SocialSolutions/Models/User.cs
@@ -46,31 +46,5 @@ namespace SocialSolutions.Models
         public IEnumerable<UsersSkills> Skills { get; set; }
 
         public IEnumerable<UsersEvents> Events { get; set; }
-
-        static public implicit operator User(ProfileViewModel model)
-        {
-            var user = new User() { Id = model.ProfileId };
-
-            return new User()
-            {
-                Id = model.ProfileId,
-                AboutMe = model.AboutMe,
-                Name = model.Name,
-                SecondName = model.SecondName,
-                Birthdate = model.Birthdate,
-                Gender = model.Gender,
-                MobilePhone = model.MobilePhone,
-                OwnCommunities = model.OwnCommunities,
-                Communities = model.Communities.Select(prop => new UsersCommunities() { User = user, Community = new Community() { Id = prop.Id } }),
-                Permits = model.Permits.Select(prop => new UsersPermits() { User = user, Permit = new Permit() { Id = prop.Id } }),
-                Documents = model.Documents.Select(prop => new Document() { Id = prop.Id }),
-                Roles = model.Roles.Select(prop => new UsersRoles() { User = user, Role = new Role() { Id = prop.Id } }),
-                Groups = model.Groups.Select(prop => new UsersGroups() { User = user, Group = new Group() { Id = prop.Id } }),
-                Hobbies = model.Hobbies.Select(prop => new UsersHobbies() { User = user, Hobby = new Hobby() { Id = prop.Id } }),
-                Skills = model.Skills.Select(prop => new UsersSkills() { User = user, Skill = new Skill() { Id = prop.Id } }),
-                Events = model.Events.Select(prop => new UsersEvents() { User = user, Event = new Event() { Id = prop.Id } }),
-                Location = model.Location
-            };
-        }
     }
 }
